fef960cbdaf0d09c4d601f8e2eddd34c8bf98245
Sam Musoni <nshutisam61@gmail.com>
2026-04-20 15:25:37 +0200

    Time: 0 ms (100%), Space: 19.2 MB (91.81%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        i, j = 0, n - 1
+        
+        while colors[0] == colors[j]:
+            j -= 1
+            
+        while colors[-1] == colors[i]:
+            i += 1
+            
+        return max(j, n - 1 - i)
